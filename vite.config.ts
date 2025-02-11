88a83694f12e4fe4b4b4ba532a51023bda0a225d
anastshak <nastyashakura@gmail.com>
2025-02-11 21:23:57 +0300

    feat: set up basic structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+        silenceDeprecations: ['import'],
+      },
+    },
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  }
 });
