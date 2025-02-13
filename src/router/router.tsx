ccaa78f9a5e53777497a7c3ae1105d8aa60e37e7
anastshak <nastyashakura@gmail.com>
2025-02-14 01:46:27 +0300

    feat: implement basic router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import { Home } from '../pages/Home/Home';
+import { NotFound } from '../pages/NotFound/NotFound';
 
 export const router = createBrowserRouter([
   {
-    path: '/home',
-    element: <App />,
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ]);
