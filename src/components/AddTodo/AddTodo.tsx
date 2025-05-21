6dfa96f88c56fe47826894b7ea21a8a232db4038
anastshak <nastyashakura@gmail.com>
2025-05-21 22:13:02 +0300

    feat: add basic components

---
+++ src/components/AddTodo/AddTodo.tsx
@@ -0,0 +1,6 @@
+import styles from './AddTodo.module.scss';
+
+//input + button
+export const AddTodo = () => {
+  return <div className={styles.itemBlock}>ADD</div>;
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,5 @@
+import styles from './TodoList.module.scss';
+
+export const TodoList = () => {
+  return <div className={styles.list}>LIST</div>;
+};
