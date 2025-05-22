bfb547e9e08ddb91ff728e8e94345c365b420786
anastshak <nastyashakura@gmail.com>
2025-05-22 21:20:07 +0300

    feat: implement add-to-do block

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,6 +1,38 @@
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './AddTodo.module.scss';
 
 //input + button
 export const AddTodo = () => {
-  return <div className={styles.itemBlock}>ADD</div>;
+  const [input, setInput] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    //focus on input
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('submit!');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.itemBlock}>
+        <input
+          ref={inputRef}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className={styles.input}
+          placeholder="start typing ..."
+        />
+        <button type="submit" className={styles.btn}>
+          add
+        </button>
+      </div>
+    </form>
+  );
 };
