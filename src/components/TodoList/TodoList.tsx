0076cb8628e6061ad9c10be948510c17f83b3aec
anastshak <nastyashakura@gmail.com>
2025-05-23 16:36:46 +0300

    feat: implement todo list component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,25 @@
+import { useTodo } from '../../context/useTodo';
+import { SiStarship } from 'react-icons/si';
+
 import styles from './TodoList.module.scss';
 
 export const TodoList = () => {
-  return <div className={styles.list}>LIST</div>;
+  const { todos } = useTodo();
+
+  if (!todos.length) {
+    return (
+      <div className={styles.msgBox}>
+        <SiStarship className={styles.icon} />
+        You have nothing to do!
+      </div>
+    );
+  }
+
+  return (
+    <ul className={styles.list}>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  );
 };
